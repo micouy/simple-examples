5f8b76a6d10ae8a7024ca76d65eaa2df16de69fe
micouy <szpontaniczny@gmail.com>
2021-09-20 13:57:18 +0200

    Format code in `legion-basic/`

--- legion-basic/src/main.rs
+++ legion-basic/src/main.rs
@@ -13,7 +13,7 @@ struct Velocity {
 }
 
 fn main() {
-	let mut world = World::default();
+    let mut world = World::default();
 
     let _ = world.extend(vec![
         (Position { x: 0.0, y: 0.0 }, Velocity { dx: 0.1, dy: 0.0 }),
@@ -21,11 +21,11 @@ fn main() {
         (Position { x: 2.0, y: 2.0 }, Velocity { dx: 0.0, dy: 0.1 }),
     ]);
 
-	for frame in 1..=3 {
-		println!("frame {}", frame);
+    for frame in 1..=3 {
+        println!("frame {}", frame);
 
-		// Update.
-    	let mut query = <(&Velocity, &mut Position)>::query()
+        // Update.
+        let mut query = <(&Velocity, &mut Position)>::query()
             .filter(maybe_changed::<Position>());
 
         for (velocity, position) in query.iter_mut(&mut world) {
@@ -33,13 +33,13 @@ fn main() {
             position.y += velocity.dy;
         }
 
-		// Draw.
-    	let mut query = <&Position>::query();
+        // Draw.
+        let mut query = <&Position>::query();
 
         for position in query.iter(&world) {
-			println!("x: {:.2}, y: {:.2}", position.x, position.y);
+            println!("x: {:.2}, y: {:.2}", position.x, position.y);
         }
 
-		println!();
-	}
+        println!();
+    }
 }
