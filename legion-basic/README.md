f552454a6a7ecd486879a3bdcd916c584416648c
micouy <szpontaniczny@gmail.com>
2021-09-20 13:54:52 +0200

    Add 'Basic `legion` example'

---
+++ legion-basic/README.md
@@ -0,0 +1,22 @@
+# Basic `legion` example
+
+**Technology**: [`legion` (`0.4.0`)](https://github.com/amethyst/legion/tree/v0.4.0), [Rust (`1.55.0-nightly`)](https://www.rust-lang.org/) 
+
+**Usage**: Run `cargo run` in the project's root. Expected output:
+
+```
+frame 1
+x: 0.10, y: 0.00
+x: 1.10, y: 1.10
+x: 2.00, y: 2.10
+
+frame 2
+x: 0.20, y: 0.00
+x: 1.20, y: 1.20
+x: 2.00, y: 2.20
+
+frame 3
+x: 0.30, y: 0.00
+x: 1.30, y: 1.30
+x: 2.00, y: 2.30
+```
---
+++ legion-basic/src/main.rs
@@ -0,0 +1,45 @@
+use legion::*;
+
+#[derive(Clone, Copy, Debug)]
+struct Position {
+    x: f32,
+    y: f32,
+}
+
+#[derive(Clone, Copy, Debug)]
+struct Velocity {
+    dx: f32,
+    dy: f32,
+}
+
+fn main() {
+	let mut world = World::default();
+
+    let _ = world.extend(vec![
+        (Position { x: 0.0, y: 0.0 }, Velocity { dx: 0.1, dy: 0.0 }),
+        (Position { x: 1.0, y: 1.0 }, Velocity { dx: 0.1, dy: 0.1 }),
+        (Position { x: 2.0, y: 2.0 }, Velocity { dx: 0.0, dy: 0.1 }),
+    ]);
+
+	for frame in 1..=3 {
+		println!("frame {}", frame);
+
+		// Update.
+    	let mut query = <(&Velocity, &mut Position)>::query()
+            .filter(maybe_changed::<Position>());
+
+        for (velocity, position) in query.iter_mut(&mut world) {
+            position.x += velocity.dx;
+            position.y += velocity.dy;
+        }
+
+		// Draw.
+    	let mut query = <&Position>::query();
+
+        for position in query.iter(&world) {
+			println!("x: {:.2}, y: {:.2}", position.x, position.y);
+        }
+
+		println!();
+	}
+}
